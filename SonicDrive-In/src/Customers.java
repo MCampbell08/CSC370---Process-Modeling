7dc7c53db7466ca61e6f34bce2ce5e8d402980c1
Mason Campbell <WaffleDefend29@gmail.com>
2017-10-14 23:02:36 -0600

    Finished Sonic Drive-In

--- SonicDrive-In/src/Customers.java
+++ SonicDrive-In/src/Customers.java
@@ -5,28 +5,76 @@ public class Customers extends Thread{
 
     public static int x = 0;
 
-    public static final int PARKING_SPOTS = 3;
+    public static final int PARKING_SPOTS = 5;
+    public static final int EMPLOYEE_AMOUNT = 3;
+    private static final int MIN_CUST_TIME = 300;
+    private static final int MAX_CUST_TIME = 500;
 
-    private static Semaphore semaphore = new Semaphore(PARKING_SPOTS);
+    private static Semaphore semaphoreParking = new Semaphore(PARKING_SPOTS);
+    private static Semaphore semaphoreEmployee = new Semaphore(EMPLOYEE_AMOUNT);
+
+    private static Object lock = new Object();
 
     private Random gen = new Random();
 
     @Override
     public void run(){
         try {
-            semaphore.acquire();
+            semaphoreParking.acquire();
         }
         catch (InterruptedException ex){throw new RuntimeException(ex);}
 
         try {
-            System.out.println("Within Employee Class");
+            System.out.println("Customer " + this.getName() + " has taken a parking spot.");
+            takeOrder();
+            deliverFood();
             int local = x;
-            pause(gen.nextInt(500) + 250);
+            System.out.println("Food is being eaten by: " + this.getName());
+            pause(gen.nextInt(MAX_CUST_TIME - MIN_CUST_TIME + 1) + MIN_CUST_TIME);
             local++;
             x = local;
         }
         finally {
-            semaphore.release();
+            semaphoreParking.release();
+            System.out.println("Customer " + this.getName() + " has released a parking spot.");
+        }
+    }
+
+    private void takeOrder(){
+
+        try {
+            semaphoreEmployee.acquire();
+        }
+        catch (InterruptedException ex){throw new RuntimeException(ex);}
+
+        try {
+            System.out.println("Taking order of Customer : " + this.getName());
+            pause(gen.nextInt(MAX_CUST_TIME - MIN_CUST_TIME + 1) + MIN_CUST_TIME);
+            synchronized (lock) {
+                System.out.println("Taking payment of Customer: " + this.getName());
+                int local = x;
+                pause(gen.nextInt(MAX_CUST_TIME - MIN_CUST_TIME + 1) + MIN_CUST_TIME);
+                local++;
+                x = local;
+                System.out.println("Finishing payment of Customer: " + this.getName());
+            }
+        }
+        finally {
+            semaphoreEmployee.release();
+        }
+    }
+
+    private void deliverFood(){
+        try{
+            semaphoreEmployee.acquire();
+        }
+        catch (InterruptedException ex){throw new RuntimeException(ex);}
+        try{
+            System.out.println("Delivering order to Customer: " + this.getName());
+            pause(gen.nextInt(MAX_CUST_TIME - MIN_CUST_TIME + 1) + MIN_CUST_TIME);
+        }
+        finally {
+            semaphoreEmployee.release();
         }
     }
 
--- SonicDrive-In/src/Main.java
+++ SonicDrive-In/src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
 
-    private static final int CUSTOMER_AMOUNT = 15;
+    private static final int CUSTOMER_AMOUNT = 10;
 
     public static void main(String[] args) {
-        Employees.x = 0;
         Customers.x = 0;
 
         for(int i = 0; i < CUSTOMER_AMOUNT; i++){
-            
+            Customers customer = new Customers();
+            customer.start();
         }
     }
 }
