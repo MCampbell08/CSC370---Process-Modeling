88fdf6e685b6deb7e6c792c039f4cea0d7e2fa6b
MCampbell08 <WaffleDefend29@gmail.com>
2017-11-07 23:47:07 -0700

    Changes

---
+++ Queueing/Queueing/Tailor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queueing
+{
+    public class Tailor
+    {
+        public static bool InUse = false;
+    }
+}
