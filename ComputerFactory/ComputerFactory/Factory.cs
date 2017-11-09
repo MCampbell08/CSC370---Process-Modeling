8e4ee83bb73857dc2d48ea5888b8b3130ea81f73
MCampbell08 <WaffleDefend29@gmail.com>
2017-11-08 19:35:03 -0700

    ComputerFactoryStart

---
+++ ComputerFactory/ComputerFactory/Factory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerFactory
+{
+    public class Factory
+    {
+        private const double WorkingHours = 0.60 * 40; // 40 hours.
+        private const float PlaceMotherboard = 0.10f, // 10 mins
+                            PlaceProcessors = 0.02f, // 2 mins
+                            PlaceHardDrive = 0.01f, //  1 min
+                            PlaceRAM = 0.0075f;    //  .75 mins
+
+        private double SimClock = 0;
+
+        private List<ComputerPart> MotherboardQueue = new List<ComputerPart>();
+
+        public void Run()
+        {
+            
+        }
+    }
+}
---
+++ ComputerFactory/ComputerFactory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComputerFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Factory().Run();
+        }
+    }
+}
