fe7725e80a7a4a2c1158da774599d67eaacd1f42
Mason Campbell <WaffleDefend29@gmail.com>
2017-11-14 12:04:52 -0700

    Added ONCLickListerner to ImageView

--- ComputerFactory/ComputerFactory/ComputerPart.cs
+++ ComputerFactory/ComputerFactory/ComputerPart.cs
@@ -6,6 +6,13 @@ namespace ComputerFactory
 {
     public class ComputerPart
     {
-
+        
+    }
+    public enum Part
+    {
+        Motherboard,
+        Processor,
+        HardDrive,
+        RAM
     }
 }
